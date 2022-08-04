3d21a073e95f076bfa93740fd3e6ab85fc55ef43
jordhanph <jordhan.emmanuel@philips.com>
2022-08-04 16:06:32 -0300

    added possiblesPalindrome function

--- src/utils/checkPalindrome.js
+++ src/utils/checkPalindrome.js
@@ -5,7 +5,7 @@
     else return false;
 }*/
 exports.__esModule = true;
-exports.howManyPalindrome = exports.isPalindrome = void 0;
+exports.possiblesPalindrome = exports.howManyPalindrome = exports.isPalindrome = void 0;
 // Check if is a Palindrome, returning true or false.
 // Ex: If parameter is 'arara', returns true, if 'teste', returns false.
 function isPalindrome(str) {
@@ -19,27 +19,64 @@ function isPalindrome(str) {
 }
 exports.isPalindrome = isPalindrome;
 // Check how many palindromes in one stirng, only palindrome of 3 characters.
-// Ex: 'asdfafadsd' will return 3 (faf, afa, dsd).
+// Ex: 'asdfafadsd' will return 3 (faf, afa, dsd) while 'asdfafadsdafa' will return 3 too, since 'afa' repeats 2 times.
 function howManyPalindrome(str) {
     if (str === void 0) { str = ''; }
     if (str.length < 3)
         return 0;
-    var strArr = str.split('');
-    var history;
+    var strArr = str.toLowerCase().split('');
+    var history = [];
     var i = 0;
     var qty = 0;
     var strVal;
     for (i; i < strArr.length - 2; i++) {
         if (strArr[i] == strArr[i + 2]) {
             strVal = strArr[i] + strArr[i + 1] + strArr[i + 2];
-            if (strArr.indexOf(strVal) == -1) { //qty = strArr.push(strVal);
+            if (history.indexOf(strVal) == -1) { //qty = strArr.push(strVal);
                 qty++;
-                strArr.push(strVal);
+                history.push(strVal);
             }
         }
     }
     return qty;
 }
 exports.howManyPalindrome = howManyPalindrome;
+function possiblesPalindrome(str) {
+    if (str === void 0) { str = ''; }
+    if (str.length < 3)
+        return 0;
+    var letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var lettersHis = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var strArr = str.toLowerCase().split('');
+    var strRev = str.toLowerCase().split('');
+    var qty = 0;
+    var index;
+    var x;
+    var y;
+    var s;
+    strRev.reverse();
+    console.log(str);
+    var _loop_1 = function (i) {
+        s = strArr[i];
+        if (lettersHis.indexOf(s) == -1)
+            return "continue"; //if the letter isn't on this array, the palindrome has already been checked for that initial letter
+        lettersHis.splice(lettersHis.indexOf(s), 1);
+        index = strRev.indexOf(s);
+        if (strArr.length - 1 - i <= index)
+            return "continue"; //see if the index found on reversed array is not the same index on non-reverse array
+        x = strArr.length - 1 - index; //x points to the same letter, like indexOf but reversed
+        letters.forEach(function (item) {
+            y = strArr.indexOf(item, i + 1);
+            if (y > i && y < x)
+                qty++;
+        });
+    };
+    for (var i = 0; i < strArr.length - 2 && lettersHis.length > 0; i++) {
+        _loop_1(i);
+    }
+    return qty;
+}
+exports.possiblesPalindrome = possiblesPalindrome;
 console.log(howManyPalindrome('1223'));
 console.log(howManyPalindrome('asdfafadsdafa'));
+console.log(possiblesPalindrome('asdfafagklopl'));
--- src/utils/checkPalindrome.ts
+++ src/utils/checkPalindrome.ts
@@ -20,23 +20,55 @@ export function isPalindrome(str:string = ''):boolean {
 export function howManyPalindrome(str:string = ''):number {
     if (str.length < 3) return 0;
 
-    let strArr:string[] = str.split('');
-    let history:string[];
+    let strArr:string[] = str.toLowerCase().split('');
+    let history:string[] = [];
     let i:number = 0;
     let qty:number = 0;
     let strVal:string;
     for (i; i<strArr.length-2; i++) {
         if (strArr[i] == strArr[i+2]) {
             strVal = strArr[i] + strArr[i+1] + strArr[i+2];
-            if (strArr.indexOf(strVal) == -1) { //qty = strArr.push(strVal);
+            if (history.indexOf(strVal) == -1) { //qty = strArr.push(strVal);
                 qty++;
-                strArr.push(strVal);
+                history.push(strVal);
             }
         }
     }
     return qty;
 }
 
+//Check how many possible palindromes in one strong, only palindrome of 3 characters.
+//Ex: 'asdfafagklopl' will return 7 (asa, ada, afa, aaa, faf, lol, lpl)
+export function possiblesPalindrome(str:string = ''):number {
+    if (str.length < 3) return 0;
+
+    let letters:string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let lettersHis:string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let strArr:string[] = str.toLowerCase().split('');
+    let strRev:string[] = str.toLowerCase().split('');
+    let qty:number = 0;
+    let index:number;
+    let x: number;
+    let y:number;
+    let s:string;
+    strRev.reverse();
+    console.log(str);
+
+    for (let i = 0; i<strArr.length-2 && lettersHis.length > 0;i++) {
+        s = strArr[i];
+        if (lettersHis.indexOf(s) == -1) continue; //if the letter isn't on this array, the palindrome has already been checked for that initial letter
+        lettersHis.splice(lettersHis.indexOf(s),1);
+        index = strRev.indexOf(s);
+        if (strArr.length - 1 - i <= index) continue; //see if the index found on reversed array is not the same index on non-reverse array
+        x = strArr.length - 1 - index; //x points to the same letter, like indexOf but reversed
+        letters.forEach(function(item) {
+            y = strArr.indexOf(item, i+1);
+            if (y > i && y < x) qty++; 
+        })
+    }
+    return qty;
+}
+
 console.log(howManyPalindrome('1223'));
 console.log(howManyPalindrome('asdfafadsdafa'));
-
+console.log(possiblesPalindrome('asdfafagklopl'));
