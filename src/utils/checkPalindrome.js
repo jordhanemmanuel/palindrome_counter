b260a224070da37845ea1ee5523adbe490377327
jordhanph <jordhan.emmanuel@philips.com>
2022-08-04 11:04:46 -0300

    add howManyPalindromes function

--- src/utils/checkPalindrome.js
+++ src/utils/checkPalindrome.js
@@ -5,7 +5,9 @@
     else return false;
 }*/
 exports.__esModule = true;
-exports.isPalindrome = void 0;
+exports.howManyPalindrome = exports.isPalindrome = void 0;
+// Check if is a Palindrome, returning true or false.
+// Ex: If parameter is 'arara', returns true, if 'teste', returns false.
 function isPalindrome(str) {
     if (str === void 0) { str = ''; }
     //you can do the same thing using only this line of code
@@ -16,5 +18,28 @@ function isPalindrome(str) {
     return str == rts ? true : false;
 }
 exports.isPalindrome = isPalindrome;
-console.log(isPalindrome('1223'));
-console.log(isPalindrome('abba'));
+// Check how many palindromes in one stirng, only palindrome of 3 characters.
+// Ex: 'asdfafadsd' will return 3 (faf, afa, dsd).
+function howManyPalindrome(str) {
+    if (str === void 0) { str = ''; }
+    if (str.length < 3)
+        return 0;
+    var strArr = str.split('');
+    var history;
+    var i = 0;
+    var qty = 0;
+    var strVal;
+    for (i; i < strArr.length - 2; i++) {
+        if (strArr[i] == strArr[i + 2]) {
+            strVal = strArr[i] + strArr[i + 1] + strArr[i + 2];
+            if (strArr.indexOf(strVal) == -1) { //qty = strArr.push(strVal);
+                qty++;
+                strArr.push(strVal);
+            }
+        }
+    }
+    return qty;
+}
+exports.howManyPalindrome = howManyPalindrome;
+console.log(howManyPalindrome('1223'));
+console.log(howManyPalindrome('asdfafadsdafa'));
--- src/utils/checkPalindrome.ts
+++ src/utils/checkPalindrome.ts
@@ -4,14 +4,39 @@
     else return false;
 }*/
 
+// Check if is a Palindrome, returning true or false.
+// Ex: If parameter is 'arara', returns true, if 'teste', returns false.
 export function isPalindrome(str:string = ''):boolean {
     //you can do the same thing using only this line of code
     //return str.split('').reverse().join('');
-    let strArr : string[] = str.split('');
+    let strArr:string[] = str.split('');
     strArr.reverse();
-    let rts : string = strArr.join('');
+    let rts:string = strArr.join('');
     return str == rts ? true : false;
 }
 
-console.log(isPalindrome('1223'));
-console.log(isPalindrome('abba'));
+// Check how many palindromes in one stirng, only palindrome of 3 characters.
+// Ex: 'asdfafadsd' will return 3 (faf, afa, dsd) while 'asdfafadsdafa' will return 3 too, since 'afa' repeats 2 times.
+export function howManyPalindrome(str:string = ''):number {
+    if (str.length < 3) return 0;
+
+    let strArr:string[] = str.split('');
+    let history:string[];
+    let i:number = 0;
+    let qty:number = 0;
+    let strVal:string;
+    for (i; i<strArr.length-2; i++) {
+        if (strArr[i] == strArr[i+2]) {
+            strVal = strArr[i] + strArr[i+1] + strArr[i+2];
+            if (strArr.indexOf(strVal) == -1) { //qty = strArr.push(strVal);
+                qty++;
+                strArr.push(strVal);
+            }
+        }
+    }
+    return qty;
+}
+
+console.log(howManyPalindrome('1223'));
+console.log(howManyPalindrome('asdfafadsdafa'));
+
